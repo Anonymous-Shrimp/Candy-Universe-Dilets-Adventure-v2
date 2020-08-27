ebfc1a5b59099360e1dafec840d3a0e3a25fa513
Anonymous Shrimp <yumiswep@gmail.com>
2020-08-26 18:43:31 -0700

    Energy Bar

--- Assets/Dilet Mechanics/MeleeSystem.cs
+++ Assets/Dilet Mechanics/MeleeSystem.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MeleeSystem : MonoBehaviour
 {
@@ -9,17 +10,21 @@ public class MeleeSystem : MonoBehaviour
     public int damage = 50;
     float Distance;
     public float maxDistance = 2;
+    [Space]
+    public Slider energyBar;
+    public float multiplier = 0.3f;
+    public float energyReduction = 0.3f;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        energyBar = GameObject.Find("EnergyBar").GetComponent<Slider>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0) && energyBar.value >= energyReduction)
         {
             RaycastHit hit;
             if(Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, Mathf.Infinity))
@@ -39,6 +44,8 @@ public class MeleeSystem : MonoBehaviour
             {
                 Miss.Play();
             }
+            energyBar.value -= energyReduction;
         }
+        energyBar.value += Time.deltaTime * multiplier;
     }
 }
