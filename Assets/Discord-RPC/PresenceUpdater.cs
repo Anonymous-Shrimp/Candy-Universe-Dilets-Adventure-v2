cf3816da32160bd99cffa8f6aa253ba84e4844de
Anonymous Shrimp <yumiswep@gmail.com>
2020-09-24 18:00:39 -0700

    Discord Rich Presence

---
+++ Assets/Discord-RPC/PresenceUpdater.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using DiscordPresence;
+using System;
+
+public class PresenceUpdater : MonoBehaviour
+{
+    public string state = "";
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Scene currentScene = SceneManager.GetActiveScene();
+        string sceneName = currentScene.name;
+        string detail = "";
+        if(sceneName == "Fire" || sceneName == "Water" || sceneName == "Thunder" || sceneName == "Rock" || sceneName == "Ice" || sceneName == "Nan")
+        {
+            state = "In " + sceneName + " Dungeon";
+        }
+        else if(sceneName == "Overworld 3")
+        {
+            state = "In Overworld";
+        }
+        else
+        {
+            state = "In " + sceneName;
+        }
+        print(state);
+        PresenceManager.UpdatePresence(detail: detail, state: state);
+    }
+}
