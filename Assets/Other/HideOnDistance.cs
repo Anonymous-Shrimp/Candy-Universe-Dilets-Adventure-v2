0f19a66db2b9bf5af29635ca59be6b3cf746f5da
Anonymous Shrimp <yumiswep@gmail.com>
2020-09-29 21:00:27 -0700

    Volumetric Lighting

---
+++ Assets/Other/HideOnDistance.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HideOnDistance : MonoBehaviour
+{
+    public Transform target;
+    public float destroyDistance = 100;
+    int layer;
+    public float distance;
+    void Start()
+    {
+        layer = gameObject.layer;
+        if (target == null && FindObjectOfType<Ouch>() != null)
+        {
+            target = FindObjectOfType<Ouch>().transform;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        distance = Vector3.Distance(transform.position, target.position);
+        if (target != null)
+        {
+            if (distance < destroyDistance)
+            {
+                gameObject.layer = layer;
+            }
+            else
+            {
+                gameObject.layer = 11;
+            }
+        }
+    }
+}
